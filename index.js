8548c180e45e5047ede1e85d6a7672ac82302a4a
jonathan <github.departure288@passmail.net>
2025-04-27 18:48:26 +0200

    test for workflow

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const { getPdfData} = require('./api');
 
 const options = {
-    key: fs.readFileSync('/etc/ssl/certs/fullchain.pem'),
-    cert: fs.readFileSync('/etc/ssl/private/privkey.pem')
+    key: fs.readFileSync('/etc/ssl/fullchain.pem'),
+    cert: fs.readFileSync('/etc/ssl/privkey.pem')
 };
 
 app.use(express.json());
