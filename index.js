ddce6135f9cd9be7201015bbf8d7c148474a87bf
jonathan <github.departure288@passmail.net>
2025-04-28 16:39:41 +0200

    test for workflow

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5173', 'https://absendo.vercel.app']
 }));
+app.options('*', cors());
 
 app.get('/', (req, res) => {
     res.send(`
